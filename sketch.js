6d987fc88b1480c575a6d1edc8f0037437a2c301
ycw310 <ycw310@nyu.edu>
2016-10-03 15:52:28 -0400

    more grasses

--- sketch.js
+++ sketch.js
@@ -57,18 +57,45 @@ function draw() {
   }
 
   // grass with varies height and color change, maybe try using array/for loop/draw randomly/custom function
+  //left bush
+  noStroke(0);
+  fill('#A6FF39');
+  triangle(o-120, 500, o- 80, 500, z-50 , k-50 );
+   triangle(o-70, 400, o- 80, 500, z-30 , k-30 );
+   triangle(o-50, 400, o-80, 500, z , k );
+   
+
+ 
+ 
 
 
-  grass(40, -40);
+  grass(30, 40);
   grass(30, 30);
   grass(40, 100);
-  grass(30, 300);
+grass(30, 300);
   grass(20, 350);
   grass(40, 200);
   grass(60, 400);
+  
+     //right bush
+noStroke(0);
+  fill('#A6FF39');
+  triangle(o, 550, o+100, 700, z+50, k+50 );
+triangle(o, 600, o+80, 700, z+80, k+80 );
+ triangle(o, 700, o+50, 700, z+100, k+100 );
+  triangle(o, 700, o+50, 700, z+130, k+130 );
+   triangle(o, 700, o+60, 800, z+140, k+140 );
+ triangle(o, 700, o+50, 700, z+150, k+150 );
 
 
-  //setting up the coordinates of the third vertex, such that the vertex moves in an arc
+//farleft bush
+ noStroke(0);
+  fill('#A6FF39');
+triangle(o-130, 450, 0- 40, 600, z-30 , k-30 );
+triangle(o-200, 500, 0-50, 700, z-30 , k-30 );
+
+
+   //setting up the coordinates of the third vertex, such that the vertex moves in an arc
   //triangle grass
   noStroke(0);
   fill('#A6FF39');
@@ -79,11 +106,30 @@ function draw() {
   triangle(o, 500, o + 30, 500, z + 20, k + 20);
   triangle(o, 500, o + 30, 500, z - 20, k - 20);
   triangle(o, 500, o + 30, 500, z - 50, k - 40);
+
   b = 0.6 * t - 0.5 * 0.01 * t * t;
-  t = t + 0.01;
+  t = t + 0.04;
   if (b > (PI / 5)) {
     t = 0;
     b = 0;
+
+    
+    /*
+    noStroke();
+colorMode(HSB, 100);
+ ellipse(mouseX,mouseY,20,20);
+for (q = 0; q < 100; q++) {
+  for (q = 0; q < 100; q++) {
+    stroke(q, j, 100);
+    point(q, j);
+   
+    function mouseIsPressed(){
+       background(146, 255, 238, 160);
+    }
+  }
+  
+  */
+
   }
 /*
   //bouncing ball
@@ -118,19 +164,13 @@ function cloud(x1, y2, w, h) {
 
 function grass(mx, g, s) { //mx=swing range,g=grassA& B point,s=scale)
   for (x = 0; x < 90; x += 10);
-  frameRate(4.5); //simulate wind speed
+  frameRate(3.5); //simulate wind speed
   var offsetA = random(-mx, mx);
   var offsetB = random(-mx, mx);
   var c = color('hsla(120, 100%, 45%, 1)');
   stroke(c);
   strokeWeight(4); //grasswidth
-  line(g + offsetA, 100, g + offsetB, 600);
-  line(g + offsetA, 100, g + offsetB, 500);
+  line(g + offsetA, 300, g + offsetB, 999);
+  line(g + offsetA, 400, g + offsetB, 600);
 
-  /*function triGrass(g2, g3, g4, g5, g6, g7) {
-    triangle(o, 500, o + 30, 500, z, k);
-    triangle(o, 500, o + 30, 500, z + 20, k + 20);
-    triangle(o, 500, o + 30, 500, z - 20, k - 20);
-
-  }*/
 }
