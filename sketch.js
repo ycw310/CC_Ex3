733ff22fe80a9e4e7ab1a801b65490e58025a2f0
ycw310 <ycw310@nyu.edu>
2016-10-03 02:05:14 -0400

    revise for moving clouds

--- sketch.js
+++ sketch.js
@@ -1,73 +1,136 @@
 function setup() {
 
   createCanvas(400, 400);
-  //background skyblue
   background(146, 255, 238, 160);
 
 }
 
+// moving clouds
+var x3 = 300;
+var y3 = 90;
+var w = 30;
+var h = 30;
+var a = 10;
+var x1speed = 3;
+var y2speed = 1;
+
+//triangle grass
+var o, z, k, b, t;
+b = 0, o = 200;
+r = 100;
+t = 0;
+
+/*
+//ball
+var l1 = 310; //ball location
+var l2 = 250; //ball location
+
+var l1speed = 0;
+var l2gravity = 0.2;
+*/
 
 function draw() {
-  // moving clouds
- /*
-  var x1 = 300;
-  var y2 = 90;
-  var w = 30;
-  var h = 30;
-
-  var x1speed = 3;
-  var y2speed = 1;
-
-  if ((x1 > width) || (x1 > 0)) {
-    x1speed = x1speed * 1;
-
-    if ((y2 > height) || (y2 > 0)) {
-      y2speed = y2speed * 2;
-      */
-    
-    //draw clouds
-    cloud(300, 90, 30, 30);
-    cloud(310, 10, 30, 30);
-    scale(0.8, 0.8);
-    cloud(200, 20, 30, 30);
+/*
+  //nobody
+  fill(0);
+  stroke(15);
+  line(300, 200, 300, 400);
+  ellipse(300, 200, 30, 30);
+  line(300, 250, 250, 200);
+  */
 
 
-    // grass with varies height and color change, maybe try using array/for loop/draw randomly/custom function
 
-    //var gray=int(random())
-    var scalar = random(0.25, 1.5);
-    grass(40, -40, scalar);
-    grass(30, 30, scalar);
-    grass(40, 100, scalar);
-    grass(30, 300, scalar);
-    grass(20, 350, scalar);
-    grass(40, 200, scalar);
-
+  //draw clouds
+  background(146, 255, 238, 160);
+  cloud(x3, y3, 30, 30);
+  cloud(310 + a, 10, 30, 30);
+  scale(0.8, 0.8);
+  cloud(200 + a * 2, 20, 30, 30);
+  x3 = x3 + 10;
+  if (x3 > 450) {
+    x3 = -80;
+  }
+  a = a + 20;
+  if (a > 200) {
+    a = -400;
   }
 
-  function cloud(x1, y2, w, h) {
-    stroke(252);
-    strokeWeight(3);
-    fill(252);
-    ellipse(x1 + 50, y2, w, h);
-    ellipse(x1 + 20, y2, w, h);
-    ellipse(x1 + 30, y2 + 19, w + 10, h + 10);
-    ellipse(x1 + 70, y2 + 40, w + 20, h + 20);
-    ellipse(x1 + 70, y2 + 10, w + 10, h + 10);
+  // grass with varies height and color change, maybe try using array/for loop/draw randomly/custom function
+
+
+  grass(40, -40);
+  grass(30, 30);
+  grass(40, 100);
+  grass(30, 300);
+  grass(20, 350);
+  grass(40, 200);
+  grass(60, 400);
+
+
+  //setting up the coordinates of the third vertex, such that the vertex moves in an arc
+  //triangle grass
+  noStroke(0);
+  fill('#A6FF39');
+
+  z = o + 15 + r * sin(b); //here the o axis moves along a circle of r radius 
+  k = 400 - r * cos(b); //here k axis moves along a circle of r radius
+  triangle(o, 500, o + 30, 500, z, k);
+  triangle(o, 500, o + 30, 500, z + 20, k + 20);
+  triangle(o, 500, o + 30, 500, z - 20, k - 20);
+  triangle(o, 500, o + 30, 500, z - 50, k - 40);
+  b = 0.6 * t - 0.5 * 0.01 * t * t;
+  t = t + 0.01;
+  if (b > (PI / 5)) {
+    t = 0;
+    b = 0;
   }
+/*
+  //bouncing ball
+  fill("#D81C0E");
+  noStroke();
+  ellipse(l1, l2, 15, 15);
 
+  l1 = l1 + l1speed;
+  l1speed = l1speed + l2gravity;
 
-  function grass(mx, g, s) { //mx=swing range,g=grassA& B point,s=scale)
-    for (x = 10; x > width > +50; x++);
-    frameRate(25); //simulate wind speed
-    var offsetA = random(-mx, mx);
-    var offsetB = random(-mx, mx);
-    var c = color('hsla(120, 100%, 45%, 0.1)');
-    stroke(c);
-    strokeWeight(4); //grasswidth
-    line(g + offsetA, 200, g - offsetB, 399);
-    scale(s); //set the scale 
+  if (l2 > height) {
+    l1speed = l1speed * -1;
 
-
-
+    l2 = height;
   }
+  */
+}
+
+
+
+function cloud(x1, y2, w, h) {
+  stroke(252);
+  strokeWeight(3);
+  fill(252);
+  ellipse(x1 + 50, y2, w, h);
+  ellipse(x1 + 20, y2, w, h);
+  ellipse(x1 + 30, y2 + 19, w + 10, h + 10);
+  ellipse(x1 + 70, y2 + 40, w + 20, h + 20);
+  ellipse(x1 + 70, y2 + 10, w + 10, h + 10);
+}
+
+
+function grass(mx, g, s) { //mx=swing range,g=grassA& B point,s=scale)
+  for (x = 0; x < 90; x += 10);
+  frameRate(4.5); //simulate wind speed
+  var offsetA = random(-mx, mx);
+  var offsetB = random(-mx, mx);
+  var c = color('hsla(120, 100%, 45%, 1)');
+  stroke(c);
+  strokeWeight(4); //grasswidth
+  line(g + offsetA, 100, g + offsetB, 600);
+  line(g + offsetA, 100, g + offsetB, 500);
+
+  /*function triGrass(g2, g3, g4, g5, g6, g7) {
+    triangle(o, 500, o + 30, 500, z, k);
+    triangle(o, 500, o + 30, 500, z + 20, k + 20);
+    triangle(o, 500, o + 30, 500, z - 20, k - 20);
+
+  }*/
+}
